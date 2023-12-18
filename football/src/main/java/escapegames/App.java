f24cefb558231fec69f74e692fddbc4b2126e49d
rebeccafitzpatr <rfit696@aucklanduni.ac.nz>
2023-12-18 20:03:44 +1300

    created project

---
+++ football/src/main/java/escapegames/App.java
@@ -0,0 +1,27 @@
+package escapegames;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Hello world!
+ */
+public final class App extends Application{
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    @Override 
+    public void start(final Stage stage) throws IOException {
+
+    }
+}
